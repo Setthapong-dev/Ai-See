4861596d0688274308b06049d11a166f63a334d4
Setthapong Thansirisuntorn <66011495@kmitl.ac.th>
2025-09-17 12:07:01 +0700

    repo ระเบิดตู้ม

--- client/pages/Ai.jsx
+++ client/pages/Ai.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Eye, MessageCircle, BarChart3 } from 'lucide-react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Ai = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
